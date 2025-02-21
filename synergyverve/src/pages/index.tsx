ceb5263857e9d6eef540add45f9070eefd29d53c
Murtaza Khan <106465242+Murtazayousufzai@users.noreply.github.com>
2025-02-21 23:25:14 +0500

    ascernity ui error

--- synergyverve/src/pages/index.tsx
+++ synergyverve/src/pages/index.tsx
@@ -2,13 +2,35 @@
 import NavBar from "../components/navbar";
 import Hero  from "../components/hero";
 import { Inter } from 'next/font/google';
-
+import {HeroParallax} from "../components/ui/HeroParallax";
+import firtsImg from "../assets/img/portfolio_img/one.jpg";
 const inter = Inter({
   subsets: ['latin'], 
   variable: '--font-inter',
   weight: ['400', '700'], 
 });
+const products = [
 
+  {
+    title: "Product 3",
+    link: "/product-3",
+    thumbnail: "/three.jpg"
+    ,
+  },
+  {
+    title: "Product 1",
+    link: "/product-1",
+    thumbnail: "/one.jpg"
+    ,
+  },
+  {
+    title: "Product 2",
+    link: "/product-2",
+    thumbnail: "/two.jpg"
+    ,
+  },
+  // Add more products as needed
+];
 
 
 export default function Home() {
@@ -16,6 +38,7 @@ export default function Home() {
   <>
   <NavBar/>
   <Hero/>
+  <HeroParallax products={products} />
   
   </>
   );
