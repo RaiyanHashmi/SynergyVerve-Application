9cb5763451f387d8f7d1427980d8f7d2446ef314
RaiyanHashmi <raiyanhaihashmi123@gmail.com>
2025-02-22 17:16:10 +0500

    Update button.tsx

--- synergyverve/src/components/button.tsx
+++ synergyverve/src/components/button.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Menu, X } from "lucide-react"; 
 import { HoverBorderGradient } from "./ui/HoverBorderGradient";
+import '@/styles/globals.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +22,7 @@ export default function NavBar() {
   return (
     <a
     href="/get-started"
-    className="px-4 py-2 bg-primary text-text rounded-lg  font-medium text-lg"
+    className="px-4 py-2 bg-primary text-text rounded-lg  font-medium text-lg col_text"
   >
     <div className=" flex justify-center text-center">
       <HoverBorderGradient
